15d4e6b0d1606f9de2e59b679aea6f8413a9dabc
Seung Bin <b0321ok@gmail.com>
2024-10-27 16:36:44 +0900

    calling error success

--- src/main/java/com/learner/studyhub/config/CorsConfig.java
+++ src/main/java/com/learner/studyhub/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://10.202.36.233:3000"); // Allowed origin
         config.addAllowedOrigin("http://localhost:3000"); // Allowed origin
-         config.addAllowedOriginPattern("https://*.ngrok-free.app"); // Allow any ngrok subdomain
+//        config.addAllowedOriginPattern("https://*.ngrok-free.app"); // Allow any ngrok subdomain
         config.addAllowedHeader("*"); // Allowed headers
         config.addAllowedMethod("*"); // Allowed methods
         config.setMaxAge(3600L); // Cache duration
